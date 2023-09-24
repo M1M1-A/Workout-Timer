8fba561c48a0fd82f8be63e356d291d75837a774
Amina <amina.begum.01@gmail.com>
2023-09-24 01:14:45 +0100

    test_initally_rounds_is_empty passes

--- backend/lib/workout_rounds.py
+++ backend/lib/workout_rounds.py
@@ -3,13 +3,13 @@ class WorkoutRounds:
   def __init__(self):
     self.rounds = []
 
-  def add_round(self, exercise_name, interval_time):
+  def add_round(self, exercise_name, round_duration):
     pass
 
   def get_all_rounds(self):
-    pass
+    return self.rounds
 
-  def edit_round(self, round_number, exercise_name, interval_time):
+  def edit_round(self, round_number, exercise_name, round_duration):
     pass
 
   def delete_all_rounds(self):
--- backend/tests/test_workout_rounds.py
+++ backend/tests/test_workout_rounds.py
@@ -1,2 +1,12 @@
 from lib.workout_rounds import WorkoutRounds
+import pytest
+
+# creating an instance of WorkoutRounds class to use for all tests
+@pytest.fixture
+def workout_rounds():
+  return WorkoutRounds()
+
+def test_initally_rounds_is_empty(workout_rounds):
+  rounds = workout_rounds.get_all_rounds()
+  assert rounds == []
 
