75b6fc657df8eabde9a0b80baafca8ac9b66724a
Amina <amina.begum.01@gmail.com>
2023-09-24 01:17:52 +0100

    test_adding_multiple_rounds passes

--- backend/tests/test_workout_rounds.py
+++ backend/tests/test_workout_rounds.py
@@ -18,3 +18,18 @@ def test_adding_round(workout_rounds):
                       "exercise_name": "Squats",
                       "round_duration": 30
                     }]
+  
+def test_adding_multiple_rounds(workout_rounds):
+  workout_rounds.add_round("Squats", 30)
+  workout_rounds.add_round("Lunges", 40)
+  rounds = workout_rounds.get_all_rounds()
+  assert rounds == [{
+                      "round_number": 1,
+                      "exercise_name": "Squats",
+                      "round_duration": 30
+                    },
+                    {
+                      "round_number": 2,
+                      "exercise_name": "Lunges",
+                      "round_duration": 40
+                    }]
